885fed610c0d87bf861dedd5a4da770f5991607e
Lucas-Mc <39505798+Lucas-Mc@users.noreply.github.com>
2019-03-14 13:19:07 -0400

    Added images

--- Matlab/Image_Processing.m
+++ Matlab/Image_Processing.m
@@ -19,31 +19,59 @@ imshow(temp_image)
 image = im2double(temp_image);
 
 %% 2. Spatial Lowpass
+h = figure(2);
+out_file = 'Lowpass.gif';
 for i = 1:0.1:15
     kernel = (1/i).*[1,1,1;1,1,1;1,1,1]; 
     new_image = convn(image, kernel);
-    figure(2)
     imshow(new_image)
     hold on
     text(10, 25, ['Kernel ratio: ',num2str(i)], 'Color', 'y', 'FontSize', 15, 'FontWeight', 'Bold', 'Interpreter', 'None');
+    drawnow 
+    % Capture the plot as an image 
+    frame = getframe(h); 
+    im = frame2im(frame); 
+    [imind,cm] = rgb2ind(im,256); 
+    % Write to the GIF File 
+    if i == 1 
+        imwrite(imind,cm,out_file,'gif', 'Loopcount',inf); 
+    else 
+        imwrite(imind,cm,out_file,'gif','WriteMode','append'); 
+    end 
 end
 
 %% 3. Spatial Highpass
+h = figure(3);
+out_file = 'Highpass.gif';
 for i = 1:0.1:15
     kernel = [0,-1,0;-1,i,-1;0,-1,0]; 
     new_image = convn(image, kernel);
-    figure(3)
     imshow(new_image)
     hold on
     text(10, 25, ['Kernel ratio: ',num2str(i)], 'Color', 'y', 'FontSize', 15, 'FontWeight', 'Bold', 'Interpreter', 'None');
+    drawnow 
+    % Capture the plot as an image 
+    frame = getframe(h); 
+    im = frame2im(frame); 
+    [imind,cm] = rgb2ind(im,256); 
+    % Write to the GIF File 
+    if i == 1 
+        imwrite(imind,cm,out_file,'gif', 'Loopcount',inf); 
+    else 
+        imwrite(imind,cm,out_file,'gif','WriteMode','append'); 
+    end 
 end
 
 %% 4. Fourier Representation
-F = fft2(image,256,256);
+h = figure(4);
+out_file = 'Fourier_Rep.png';
+F = fft2(image,512,512);
 F2 = fftshift(F);
 imshow(log(1+abs(F2)));
 colormap(jet); 
 colorbar
+%drawnow
+saveas(h,out_file);
 
 %% 5. Fourier Lowpass
 
